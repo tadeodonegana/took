4bbdc10f4592029abd0261a207dbbdaf429390c2
tadeodonegana <tadeodonegana@gmail.com>
2022-06-01 00:10:05 -0300

    Book preprocessing

--- text_to_tweets.py
+++ text_to_tweets.py
@@ -1,11 +1,11 @@
 import nltk
 
-# Uncomment line 5 if you're running the code for the first time, then comment it again.
+# Comment line 5 if you're running the code not for the first time.
 # More info about nltk.download() in https://www.nltk.org/api/nltk.html?highlight=nltk%20download#module-nltk.downloader
 nltk.download('punkt')
 
 # Converts a .txt to a list of sentences to tweet
-def txt_to_list_of_tweets(txt_name):
+def txt_to_list_of_sentences(txt_name):
     with open(txt_name, 'r') as file:
         data = file.read().replace('\n', ' ')
     return nltk.tokenize.sent_tokenize(data)
@@ -14,3 +14,18 @@ def txt_to_list_of_tweets(txt_name):
 def make_sentence_available_for_tweet(sentence):
     sentence = sentence.replace(",", ".")
     return nltk.tokenize.sent_tokenize(sentence)
+
+# Generates a txt file within each line is a tweet ready to be sent
+def tweetify_book(txt_name):
+    list_of_sentences = txt_to_list_of_sentences('books/'+txt_name)
+    
+    with open('tweetify-books/tweetify-'+txt_name, 'w') as file:
+        for sentence in list_of_sentences:
+            if(len(sentence) >= 280):
+                long_sentences = make_sentence_available_for_tweet(sentence)
+                for long_sentence in long_sentences:
+                    file.write(long_sentence+'\n')
+            else:
+                file.write(sentence+'\n')
+
+tweetify_book('the-last-question-asimov.txt')
